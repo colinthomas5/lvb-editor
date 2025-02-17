ce6fc3a0897a30ae4c8e01b1949978bee061b722
colin <colinerickson@live.com>
2025-02-16 23:31:09 -0500

    entitiy object attributes stored as hex values

--- main.py
+++ main.py
@@ -5,39 +5,39 @@ header = file.read(128)
 
 class Entity:
     def __init__(self, layer, offset):
-        self.offset = offset
+        self.offset = hex(offset)
         self.layer = layer
         file.seek(offset, 0)
-        self.type = file.read(8)
-        self.unknown1 = file.read(4)
+        self.type = file.read(8).hex()
+        self.unknown1 = file.read(4).hex()
         index = file.read(4)
-        self.index = index
-        self.unknown2 = file.read(4)
-        self.posX = file.read(4)
-        self.posY = file.read(4)
-        self.posZ = file.read(4)
-        self.stretchX = file.read(4)
-        self.unknown4 = file.read(4)
-        self.unknown5 = file.read(4)
-        self.unknown6 = file.read(4)
-        self.unknown7 = file.read(4)
-        self.unknown8 = file.read(4)
-        self.unknown9 = file.read(4)
-        self.unknown10 = file.read(4)
-        self.headerEnd = file.read(4)
+        self.index = index.hex
+        self.unknown2 = file.read(4).hex()
+        self.posX = file.read(4).hex()
+        self.posY = file.read(4).hex()
+        self.posZ = file.read(4).hex()
+        self.stretchX = file.read(4).hex()
+        self.unknown4 = file.read(4).hex()
+        self.unknown5 = file.read(4).hex()
+        self.unknown6 = file.read(4).hex()
+        self.unknown7 = file.read(4).hex()
+        self.unknown8 = file.read(4).hex()
+        self.unknown9 = file.read(4).hex()
+        self.unknown10 = file.read(4).hex()
+        self.headerEnd = file.read(4).hex()
         nameLocation = int.from_bytes(header[72:76], "little")+((int.from_bytes(index, "little"))*8)
-        self.nameLocation = nameLocation
+        self.nameLocation = hex(nameLocation)
         file.seek(nameLocation, 0)
         nameOffset = int.from_bytes(file.read(8), "little")
-        self.nameOffset = nameOffset
+        self.nameOffset = hex(nameOffset)
         file.seek(nameOffset, 0)
         nameLength = int.from_bytes(file.read(4), "little")
-        self.nameLength = nameLength
+        self.nameLength = hex(nameLength)
         self.name = file.read(nameLength)
 
 class Layer:
     def __init__(self, offset, numberOfEntities, entityList):
-        self.offset = offset
+        self.offset = hex(offset)
         self.numberOfEntities = numberOfEntities
         self.entityList = entityList
         entityNumber=0

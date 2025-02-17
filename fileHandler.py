e332c0550f949f1b68a7c2709bbb260137ea8da0
colin <colinerickson@live.com>
2025-02-17 18:05:53 -0500

    created new main.py to run tkinter application window; repurposed old main.py into fileHandler.py; new main.py now displays parsed layers, entities, and entity properties in a gui

---
+++ fileHandler.py
@@ -0,0 +1,79 @@
+# User is prompted to choose the .lvb file to open
+# filePath = input("Enter file directory: ")
+# file = open(filePath, 'rb')
+
+def openLVBFile(LVBFile):
+
+    global file
+    file = LVBFile
+
+    # Header of .lvb file is separated
+    global header
+    header = file.read(128)
+    
+    # Every .lvb file has four layers where entities may exist. They also have a fifth layer, which is dedicated to storing the names of all of the entities in the .lvb file. These lists anticipate that there will only ever be four layers (plus the name layer).
+    entityList1 = []
+    entityList2 = []
+    entityList3 = []
+    entityList4 = []
+    # entityList5 = []
+
+    # Similar to the lists above, the creation of these four object layers are made under the assumption that there will always be four entity layers (and one name layer).
+    layer1 = Layer(int.from_bytes(header[8:12], "little"), int.from_bytes(header[4:8], "little"), entityList1)
+    layer2 = Layer(int.from_bytes(header[24:28], "little"), int.from_bytes(header[16:20], "little"), entityList2)
+    layer3 = Layer(int.from_bytes(header[40:44], "little"), int.from_bytes(header[32:36], "little"), entityList3)
+    layer4 = Layer(int.from_bytes(header[56:60], "little"), int.from_bytes(header[48:52], "little"), entityList4)
+    layerList = [layer1, layer2, layer3, layer4]
+
+    return layerList
+    # layer5 = Layer(int.from_bytes(header[72:76], "little"), int.from_bytes(header[64:68], "little"), entityList5)
+
+# Entity objects represent all of the entities that are within the .lvb files. Regardless of type, entities all share the same header format. Different entity types will have different data following their "headerEnd", which should always be "FFFFFFFF"
+class Entity:
+    def __init__(self, layer, offset):
+        self.offset = hex(offset)
+        self.layer = layer
+        file.seek(offset, 0)
+        self.type = file.read(8).hex()
+        self.unknown1 = file.read(4).hex()
+        index = file.read(4)
+        self.index = index.hex()
+        self.unknown2 = file.read(4).hex()
+        self.posX = file.read(4).hex()
+        self.posY = file.read(4).hex()
+        self.posZ = file.read(4).hex()
+        self.stretchX = file.read(4).hex()
+        self.unknown4 = file.read(4).hex()
+        self.unknown5 = file.read(4).hex()
+        self.unknown6 = file.read(4).hex()
+        self.unknown7 = file.read(4).hex()
+        self.unknown8 = file.read(4).hex()
+        self.unknown9 = file.read(4).hex()
+        self.unknown10 = file.read(4).hex()
+        self.headerEnd = file.read(4).hex()
+        nameLocation = int.from_bytes(header[72:76], "little")+((int.from_bytes(index, "little"))*8)
+        self.nameLocation = hex(nameLocation)
+        file.seek(nameLocation, 0)
+        nameOffset = int.from_bytes(file.read(8), "little")
+        self.nameOffset = hex(nameOffset)
+        file.seek(nameOffset, 0)
+        nameLength = int.from_bytes(file.read(4), "little")
+        self.nameLength = hex(nameLength)
+        self.name = file.read(nameLength)
+
+# Entities are sorted into Layers within .lvb files. Layers start with a table that lists all hex offsets of entities within that layer, followed by the entities. When parsing a layer, the layer object fills a list with all of the entities in that layer while populating all of the information about the entities.
+class Layer:
+    def __init__(self, offset, numberOfEntities, entityList):
+        self.offset = hex(offset)
+        self.numberOfEntities = numberOfEntities
+        self.entityList = entityList
+        entityNumber=0
+        while(entityNumber < numberOfEntities):
+            entityLocation = offset+(entityNumber*8)
+            file.seek(entityLocation, 0)
+            entityOffset = int.from_bytes(file.read(8), "little")
+            entity = Entity(self, entityOffset)
+            entityList.append(entity)
+            entityNumber+=1
+
+
--- main.py
+++ main.py
@@ -1,62 +1,292 @@
-filePath = input("Enter file directory: ")
-file = open(filePath, 'rb')
+import os
+import fileHandler
+from tkinter import *
+from tkinter import filedialog
 
-header = file.read(128)
+# Creating root window
+root = Tk()
+root.title("LVB-Edit: No File")
+layerList = []
 
-class Entity:
-    def __init__(self, layer, offset):
-        self.offset = hex(offset)
-        self.layer = layer
-        file.seek(offset, 0)
-        self.type = file.read(8).hex()
-        self.unknown1 = file.read(4).hex()
-        index = file.read(4)
-        self.index = index.hex
-        self.unknown2 = file.read(4).hex()
-        self.posX = file.read(4).hex()
-        self.posY = file.read(4).hex()
-        self.posZ = file.read(4).hex()
-        self.stretchX = file.read(4).hex()
-        self.unknown4 = file.read(4).hex()
-        self.unknown5 = file.read(4).hex()
-        self.unknown6 = file.read(4).hex()
-        self.unknown7 = file.read(4).hex()
-        self.unknown8 = file.read(4).hex()
-        self.unknown9 = file.read(4).hex()
-        self.unknown10 = file.read(4).hex()
-        self.headerEnd = file.read(4).hex()
-        nameLocation = int.from_bytes(header[72:76], "little")+((int.from_bytes(index, "little"))*8)
-        self.nameLocation = hex(nameLocation)
-        file.seek(nameLocation, 0)
-        nameOffset = int.from_bytes(file.read(8), "little")
-        self.nameOffset = hex(nameOffset)
-        file.seek(nameOffset, 0)
-        nameLength = int.from_bytes(file.read(4), "little")
-        self.nameLength = hex(nameLength)
-        self.name = file.read(nameLength)
+# Command for opening a .lvb file
+def openFile():
+    filePath = filedialog.askopenfilename(title="Select .lvb file", filetypes=[("*.lvb", ".lvb")])
+    fileName = os.path.split(filePath)[1]
+    global file
+    file = open(filePath, 'rb')
+    layerListbox.delete(0, END)
+    entityListbox.delete(0, END)
+    global layerList
+    layerList = fileHandler.openLVBFile(file)
+    layerNumber=1
+    for Layer in layerList:
+        layerListbox.insert(END, "Layer "+str(layerNumber))
+        layerNumber+=1
+    layerListbox.select_set(0)
+    layerListbox.event_generate("<<ListboxSelect>>")
+    root.title("LVB-Edit: "+fileName)
+    #currentFileLabel.config(text="Current file: "+fileName)
 
-class Layer:
-    def __init__(self, offset, numberOfEntities, entityList):
-        self.offset = hex(offset)
-        self.numberOfEntities = numberOfEntities
-        self.entityList = entityList
-        entityNumber=0
-        while(entityNumber < numberOfEntities):
-            entityLocation = offset+(entityNumber*8)
-            file.seek(entityLocation, 0)
-            entityOffset = int.from_bytes(file.read(8), "little")
-            entity = Entity(self, entityOffset)
-            entityList.append(entity)
-            entityNumber+=1
+# Command for closing the currently-open .lvb file
+def closeFile():
+    global file
+    file.close()
+    layerListbox.delete(0, END)
+    entityListbox.delete(0, END)
+    root.title("LVB-Edit: No File")
+    #currentFileLabel.config(text="Current file: ")
 
-entityList1 = []
-entityList2 = []
-entityList3 = []
-entityList4 = []
-# entityList5 = []
+# Frame that holds open and close file buttons
+fileButtonFrame = LabelFrame(root, padx=5, pady=5)
+fileButtonFrame.grid(row=0, column=1, sticky="W")
 
-layer1 = Layer(int.from_bytes(header[8:12], "little"), int.from_bytes(header[4:8], "little"), entityList1)
-layer2 = Layer(int.from_bytes(header[24:28], "little"), int.from_bytes(header[16:20], "little"), entityList2)
-layer3 = Layer(int.from_bytes(header[40:44], "little"), int.from_bytes(header[32:36], "little"), entityList3)
-layer4 = Layer(int.from_bytes(header[56:60], "little"), int.from_bytes(header[48:52], "little"), entityList4)
-# layer5 = Layer(int.from_bytes(header[72:76], "little"), int.from_bytes(header[64:68], "little"), entityList5)
+# Button to open a .lvb file
+openFileButton = Button(fileButtonFrame, text="Open File", state=NORMAL, padx=30, pady=10, command=openFile)
+openFileButton.grid(row=0, column=0)
+
+# Button to close a .lvb file
+closeFileButton = Button(fileButtonFrame, text="Close File", state=NORMAL, padx=30, pady=10, command=closeFile)
+closeFileButton.grid(row=0, column=1)
+
+# Text that reads what file is currently open. Deprecated in favor of showing currently-open file in window name
+#currentFileLabel = Label(root, text="Current file: ")
+#currentFileLabel.grid(row=1, column=1, sticky="W")
+
+# Frame that shows all entity layers
+layerFrame = LabelFrame(root, text="Layers", padx=5, pady=5)
+layerFrame.grid(row=2, column=0, sticky="W")
+
+# Frame that shows all entities in a layer
+entityFrame = LabelFrame(root, text="Entities", padx=5, pady=5)
+entityFrame.grid(row=2, column=1, sticky="W")
+
+# Causes the values Entry widgets to populate with information about each Entity. This is long and messy.
+def onEntitySelect(self):
+    global selectedLayer
+    if entityListbox.curselection():
+        valueNameEntry.delete(0, END)
+        valueTypeEntry.delete(0, END)
+        valueUnknown1Entry.delete(0, END)
+        valueIndexEntry.delete(0, END)
+        valueUnknown2Entry.delete(0, END)
+        valuePosXEntry.delete(0, END)
+        valuePosYEntry.delete(0, END)
+        valuePosZEntry.delete(0, END)
+        valueStretchXEntry.delete(0, END)
+        valueUnknown4Entry.delete(0, END)
+        valueUnknown5Entry.delete(0, END)
+        valueUnknown6Entry.delete(0, END)
+        valueUnknown7Entry.delete(0, END)
+        valueUnknown8Entry.delete(0, END)
+        valueUnknown9Entry.delete(0, END)
+        valueUnknown10Entry.delete(0, END)
+        valueHeaderEndEntry.delete(0, END)
+    for selection in entityListbox.curselection():
+        currentEntity = selectedLayer.entityList[selection]
+        valueNameEntry.insert(0, currentEntity.name)
+        valueTypeEntry.insert(1, currentEntity.type)
+        valueUnknown1Entry.insert(2, currentEntity.unknown1)
+        valueIndexEntry.insert(3, currentEntity.index)
+        valueUnknown2Entry.insert(4, currentEntity.unknown2)
+        valuePosXEntry.insert(5, currentEntity.posX)
+        valuePosYEntry.insert(6, currentEntity.posY)
+        valuePosZEntry.insert(7, currentEntity.posZ)
+        valueStretchXEntry.insert(8, currentEntity.stretchX)
+        valueUnknown4Entry.insert(9, currentEntity.unknown4)
+        valueUnknown5Entry.insert(10, currentEntity.unknown5)
+        valueUnknown6Entry.insert(11, currentEntity.unknown6)
+        valueUnknown7Entry.insert(12, currentEntity.unknown7)
+        valueUnknown8Entry.insert(13, currentEntity.unknown8)
+        valueUnknown9Entry.insert(14, currentEntity.unknown9)
+        valueUnknown10Entry.insert(15, currentEntity.unknown10)
+        valueHeaderEndEntry.insert(16, currentEntity.headerEnd)
+
+# Listbox that shows all entity layers
+entityListbox = Listbox(entityFrame, width=40, height=20)
+entityListbox.grid(row=0, column=1, sticky="W")
+entityListbox.bind("<<ListboxSelect>>", onEntitySelect)
+
+entityScrollbar = Scrollbar(entityFrame, orient="vertical", command=entityListbox.yview)
+entityScrollbar.grid(row=0, column=0, sticky="NS")
+entityListbox.configure(yscrollcommand=entityScrollbar.set)
+
+# Causes the Entity listbox to populate with entities from a given layer when a layer is selected in the Layer listbox
+def onLayerSelect(self):
+    global layerList
+    if layerListbox.curselection():
+        entityListbox.delete(0, END)
+        for selection in layerListbox.curselection():
+            currentLayer = int(selection)
+            global selectedLayer
+            selectedLayer = layerList[currentLayer]
+            for Entity in selectedLayer.entityList:
+                entityListbox.insert(END, Entity.name)
+
+# Listbox that shows all entity layers
+layerListbox = Listbox(layerFrame, width=10, height=20)
+layerListbox.grid(row=0, column=0, sticky="W")
+layerListbox.bind("<<ListboxSelect>>", onLayerSelect)
+
+propertyFrame = LabelFrame(root, text="Properties", padx=5, pady=4)
+propertyFrame.grid(row=2, column=2, sticky="W")
+
+# List of properties that each entity has despite type
+propertyLabel = Label(propertyFrame, text="Property", width=8)
+propertyLabel.grid(row=0, column=0, sticky="NW")
+propertyLabelFrame = LabelFrame(propertyFrame, width=8)
+propertyLabelFrame.grid(row=1, column=0, sticky="NS")
+
+# Show list of properties next to the editable values. This method sucks and uses so many lines but sadly using a listbox has the properties and values not line up so this was my best implementation of this.
+propertyNameEntry = Entry(propertyLabelFrame, width=12)
+propertyNameEntry.insert(0, "Name")
+propertyNameEntry.bind("<Key>", lambda e: "break")
+propertyNameEntry.pack()
+
+propertyTypeEntry = Entry(propertyLabelFrame, width=12)
+propertyTypeEntry.insert(1, "Type")
+propertyTypeEntry.bind("<Key>", lambda e: "break")
+propertyTypeEntry.pack()
+
+propertyUnknown1Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown1Entry.insert(2, "Unknown1")
+propertyUnknown1Entry.bind("<Key>", lambda e: "break")
+propertyUnknown1Entry.pack()
+
+propertyIndexEntry = Entry(propertyLabelFrame, width=12)
+propertyIndexEntry.insert(3, "Index")
+propertyIndexEntry.bind("<Key>", lambda e: "break")
+propertyIndexEntry.pack()
+
+propertyUnknown2Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown2Entry.insert(4, "Unknown2")
+propertyUnknown2Entry.bind("<Key>", lambda e: "break")
+propertyUnknown2Entry.pack()
+
+propertyPosXEntry = Entry(propertyLabelFrame, width=12)
+propertyPosXEntry.insert(5, "PosX")
+propertyPosXEntry.bind("<Key>", lambda e: "break")
+propertyPosXEntry.pack()
+
+propertyPosYEntry = Entry(propertyLabelFrame, width=12)
+propertyPosYEntry.insert(6, "PosY")
+propertyPosYEntry.bind("<Key>", lambda e: "break")
+propertyPosYEntry.pack()
+
+propertyPosZEntry = Entry(propertyLabelFrame, width=12)
+propertyPosZEntry.insert(7, "PosZ")
+propertyPosZEntry.bind("<Key>", lambda e: "break")
+propertyPosZEntry.pack()
+
+propertyStretchXEntry = Entry(propertyLabelFrame, width=12)
+propertyStretchXEntry.insert(8, "StretchX")
+propertyStretchXEntry.bind("<Key>", lambda e: "break")
+propertyStretchXEntry.pack()
+
+propertyUnknown4Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown4Entry.insert(9, "Unknown4")
+propertyUnknown4Entry.bind("<Key>", lambda e: "break")
+propertyUnknown4Entry.pack()
+
+propertyUnknown5Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown5Entry.insert(10, "Unknown5")
+propertyUnknown5Entry.bind("<Key>", lambda e: "break")
+propertyUnknown5Entry.pack()
+
+propertyUnknown6Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown6Entry.insert(11, "Unknown6")
+propertyUnknown6Entry.bind("<Key>", lambda e: "break")
+propertyUnknown6Entry.pack()
+
+propertyUnknown7Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown7Entry.insert(12, "Unknown7")
+propertyUnknown7Entry.bind("<Key>", lambda e: "break")
+propertyUnknown7Entry.pack()
+
+propertyUnknown8Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown8Entry.insert(13, "Unknown8")
+propertyUnknown8Entry.bind("<Key>", lambda e: "break")
+propertyUnknown8Entry.pack()
+
+propertyUnknown9Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown9Entry.insert(14, "Unknown9")
+propertyUnknown9Entry.bind("<Key>", lambda e: "break")
+propertyUnknown9Entry.pack()
+
+propertyUnknown10Entry = Entry(propertyLabelFrame, width=12)
+propertyUnknown10Entry.insert(15, "Unknown10")
+propertyUnknown10Entry.bind("<Key>", lambda e: "break")
+propertyUnknown10Entry.pack()
+
+propertyHeaderEndEntry = Entry(propertyLabelFrame, width=12)
+propertyHeaderEndEntry.insert(16, "HeaderEnd")
+propertyHeaderEndEntry.bind("<Key>", lambda e: "break")
+propertyHeaderEndEntry.pack()
+
+
+'''# Listbox that shows all properties that are applicable to all entities, regardles of type
+propertyListbox = Listbox(propertyFrame, width=20, height=19)
+propertyListbox.insert(0, "Name")
+propertyListbox.insert(1, "Type")
+propertyListbox.insert(2, "Unknown1")
+propertyListbox.insert(3, "Index")
+propertyListbox.insert(4, "Unknown2")
+propertyListbox.insert(5, "PositionX")
+propertyListbox.insert(6, "PositionY")
+propertyListbox.insert(7, "PositionZ")
+propertyListbox.insert(8, "StretchX")
+propertyListbox.insert(9, "Unknown4")
+propertyListbox.insert(10, "Unknown5")
+propertyListbox.insert(11, "Unknown6")
+propertyListbox.insert(12, "Unknown7")
+propertyListbox.insert(13, "Unknown8")
+propertyListbox.insert(14, "Unknown9")
+propertyListbox.insert(15, "Unknown10")
+propertyListbox.insert(16, "HeaderEnd")
+propertyListbox.grid(row=1, column=0, sticky="W")'''
+
+# List of Entries for viewing and modifying entity properties. This is also messy like the property Entries but maybe I'll fix it when Nova yells at me later about it. (Hi Nova :3)
+valueLabel = Label(propertyFrame, text="Value")
+valueLabel.grid(row=0, column=1)
+valueFrame = LabelFrame(propertyFrame)
+valueFrame.grid(row=1, column=1, sticky="NSW")
+valueNameEntry = Entry(valueFrame, width=30)
+valueNameEntry.pack()
+valueTypeEntry = Entry(valueFrame, width=30)
+valueTypeEntry.pack()
+valueUnknown1Entry = Entry(valueFrame, width=30)
+valueUnknown1Entry.pack()
+valueIndexEntry = Entry(valueFrame, width=30)
+valueIndexEntry.pack()
+valueUnknown2Entry = Entry(valueFrame, width=30)
+valueUnknown2Entry.pack()
+valuePosXEntry = Entry(valueFrame, width=30)
+valuePosXEntry.pack()
+valuePosYEntry = Entry(valueFrame, width=30)
+valuePosYEntry.pack()
+valuePosZEntry = Entry(valueFrame, width=30)
+valuePosZEntry.pack()
+valueStretchXEntry = Entry(valueFrame, width=30)
+valueStretchXEntry.pack()
+valueUnknown4Entry = Entry(valueFrame, width=30)
+valueUnknown4Entry.pack()
+valueUnknown5Entry = Entry(valueFrame, width=30)
+valueUnknown5Entry.pack()
+valueUnknown6Entry = Entry(valueFrame, width=30)
+valueUnknown6Entry.pack()
+valueUnknown7Entry = Entry(valueFrame, width=30)
+valueUnknown7Entry.pack()
+valueUnknown8Entry = Entry(valueFrame, width=30)
+valueUnknown8Entry.pack()
+valueUnknown9Entry = Entry(valueFrame, width=30)
+valueUnknown9Entry.pack()
+valueUnknown10Entry = Entry(valueFrame, width=30)
+valueUnknown10Entry.pack()
+valueHeaderEndEntry = Entry(valueFrame, width=30)
+valueHeaderEndEntry.pack()
+
+#valueListbox = Listbox(propertyFrame, width=20, height=19)
+#valueListbox.insert(0, "Name")
+#valueListbox.grid(row=1, column=1, sticky="W")
+
+# Main loop of program
+root.mainloop()
